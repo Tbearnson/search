6924e43519f57445522e0244f355b37e7877cfcc
Tanner Bindrup <tbindrup1221@gmail.com>
2016-07-13 17:17:19 -0600

    first shot at searching album data

--- directives/search/search.js
+++ directives/search/search.js
@@ -21,10 +21,15 @@
 		
 		$scope.$on('SwiftSearchData:artists ready', function(e, artists) {
 			sc.loading.artists = false;
-			$scope.$apply(function(){sc.artists = SwiftSearchData.getSwiftSearchResults(sc.search_text);});
+			$scope.$apply(function(){sc.artists = SwiftSearchData.getSwiftArtistResults(sc.search_text);});
+		});
+		$scope.$on('SwiftSearchData:tracks ready', function(e, artists) {
+			sc.loading.songs = false;
+			$scope.$apply(function(){sc.songs = SwiftSearchData.getSwiftTrackResults(sc.search_text);});
 		});
 		sc.getSwiftSearchResults = function() {
-			if (sc.search_text) sc.artists = SwiftSearchData.getSwiftSearchResults(sc.search_text);
+			if (sc.search_text) sc.artists = SwiftSearchData.getSwiftArtistResults(sc.search_text);
+			if (sc.search_text) sc.songs = SwiftSearchData.getSwiftTrackResults(sc.search_text);
 		};
 
 		sc.goToDetail = function(artist) {
@@ -37,21 +42,42 @@
 		var TheSwiftSearchData = {};
 		var publicAPI = {};
 
-		function preloadSwiftSearchData() {
+		function preloadSwiftArtistsData() {
 			domo.get('/data/v1/artists?groupby=artist_name&orderby=artist_name')
 			.then(function(artists_data){
 				TheSwiftSearchData.artists = artists_data;
 				$rootScope.$broadcast('SwiftSearchData:artists ready', artists_data);
 			});
 		}
-		preloadSwiftSearchData();
+		preloadSwiftArtistsData();
 
-		publicAPI.getSwiftSearchResults = function(query_string) {
+		function preloadSwiftTracksData() {
+			domo.get('/data/v1/tracks?groupby=resource_title&max=canopus_artist_name&limit=100000&orderby=resource_title')
+			.then(function(tracks_data){
+				console.log(tracks_data);
+				TheSwiftSearchData.tracks = tracks_data;
+				$rootScope.$broadcast('SwiftSearchData:tracks ready', tracks_data);
+			});
+		}
+		preloadSwiftTracksData();
+
+
+
+		publicAPI.getSwiftArtistResults = function(query_string) {
 			if (query_string && TheSwiftSearchData.artists) return _.sortBy(TheSwiftSearchData.artists.filter(function(item) {
 				return item.artist_name.toLowerCase().indexOf(query_string.toLowerCase()) > -1;
 			}), function(item){return item.artist_name.toLowerCase().indexOf(query_string.toLowerCase());}).slice(0,25);
 
 			else if (TheSwiftSearchData.artists) return TheSwiftSearchData.artists.sort().slice(0,25);
+
+			else return [];
+		}
+		publicAPI.getSwiftTrackResults = function(query_string) {
+			if (query_string && TheSwiftSearchData.tracks) return _.sortBy(TheSwiftSearchData.tracks.filter(function(item) {
+				return item.resource_title.toLowerCase().indexOf(query_string.toLowerCase()) > -1;
+			}), function(item){return item.resource_title.toLowerCase().indexOf(query_string.toLowerCase());}).slice(0,25);
+
+			else if (TheSwiftSearchData.tracks) return TheSwiftSearchData.tracks.sort().slice(0,25);
 
 			else return [];
 		}
